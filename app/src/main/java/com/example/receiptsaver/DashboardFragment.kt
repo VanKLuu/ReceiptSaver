393057c9ead12a7645599992f7034f28a78a1c08
No-Name-123-svg <daoducthienan007@gmail.com>
2024-02-29 14:57:45 -0800

    update file

--- app/src/main/java/com/example/receiptsaver/DashboardFragment.kt
+++ app/src/main/java/com/example/receiptsaver/DashboardFragment.kt
@@ -1,31 +1,24 @@
 package com.example.receiptsaver
 
+import android.app.Activity
 import android.content.Intent
-import android.content.res.Configuration
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.widget.SearchView
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.receiptsaver.db.MyDatabaseRepository
 import com.example.receiptsaver.db.Receipts
 import java.io.InputStream
-import android.app.Activity
 import java.util.UUID
 import java.util.concurrent.Executors
+import android.widget.Button
 
-
-private const val TAG = "DashboardFragment"
+private const val LOG_TAG = "DashboardFragment"
 private const val REQUEST_PDF_SELECTION = 2
 
 class DashboardFragment : Fragment() {
@@ -38,7 +31,9 @@ class DashboardFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate called from DashboardFragment")
+        Log.d(LOG_TAG, "onCreate called from DashboardFragment")
+        dbRepo = MyDatabaseRepository(requireContext())
+        adapter = ReceiptAdapter(emptyList())
     }
 
     override fun onCreateView(
@@ -46,46 +41,35 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.e(TAG, "onCreateView called from DashboardFragment")
+        Log.e(LOG_TAG, "onCreateView called from DashboardFragment")
         val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        dbRepo = MyDatabaseRepository(requireContext())
-
-        // Initialize views and set up RecyclerView adapter
         initializeViews(view)
-
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // Set up click listener for upload button
         uploadButton.setOnClickListener {
             openPdfPicker()
         }
-
-        // Set up query text listener for search functionality
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                // Handle search query submission
                 return false
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                // Handle search query text change
                 return false
             }
         })
+        updateUI()
     }
 
     private fun initializeViews(view: View) {
         uploadButton = view.findViewById(R.id.buttonUpload)
         searchView = view.findViewById(R.id.searchView)
         receiptRecyclerView = view.findViewById(R.id.recyclerViewReceipts)
-        adapter = ReceiptAdapter(emptyList())
         receiptRecyclerView.adapter = adapter
         receiptRecyclerView.layoutManager = GridLayoutManager(context, 2)
-        updateUI()
     }
 
     private fun openPdfPicker() {
@@ -104,7 +88,7 @@ class DashboardFragment : Fragment() {
                     inputStream?.close()
                     pdfByteArray?.let { savePdfToDatabase(it) }
                 } catch (e: Exception) {
-                    Log.e(TAG, "Error reading PDF file", e)
+                    Log.e(LOG_TAG, "Error reading PDF file", e)
                 }
             }
         }
@@ -125,7 +109,9 @@ class DashboardFragment : Fragment() {
 
     private fun updateUI() {
         dbRepo.fetchAllReceipts().observe(viewLifecycleOwner) { receiptList ->
-            adapter?.updateReceipts(receiptList)
+            Log.e(LOG_TAG, "albumList observe called with receiptList=$receiptList")
+            adapter = receiptList?.let { ReceiptAdapter(it) }
+            this.receiptRecyclerView.adapter = adapter
         }
     }
 
@@ -137,7 +123,6 @@ class DashboardFragment : Fragment() {
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReceiptHolder {
-            // Inflate layout and create ViewHolder
             return ReceiptHolder(LayoutInflater.from(parent.context).inflate(R.layout.receipt_item, parent, false))
         }
 
@@ -159,4 +144,3 @@ class DashboardFragment : Fragment() {
         }
     }
 }
-
